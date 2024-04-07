fa5184c3c1712db8096da72173dbf1c73c71d861
amr-hc <amro7275@gmail.com>
2024-04-07 21:05:34 +0200

    user & project & category api

---
+++ backend/crowdfunding/api/urls.py
@@ -0,0 +1,23 @@
+
+
+from django.urls import path,include
+from api.views import testview,UserListCreateAPIView,LoginAPIView,CategoryListCreateAPIView,ProjectListCreateAPIView,ProjectModelViewSet
+
+
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'projects', ProjectModelViewSet)
+
+urlpatterns = [
+    path("users", UserListCreateAPIView.as_view()),
+    path("categories", CategoryListCreateAPIView.as_view()),
+    path("projects", ProjectListCreateAPIView.as_view()),
+    path('', include(router.urls)),
+    path('login/', include('django.contrib.auth.urls')),
+    path('loginv/', LoginAPIView.as_view(), name='login'),
+
+]
+
+
+
+
--- backend/crowdfunding/crowdfunding/settings.py
+++ backend/crowdfunding/crowdfunding/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_cleanup.apps.CleanupConfig',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'crowdfunding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
+
+import os
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
