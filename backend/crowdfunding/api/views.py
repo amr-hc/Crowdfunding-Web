2a0178acfc829b9229f7a4a35d6b76810cfd8f47
Ameer Elattar <amer.alattar.ae@gmail.com>
2024-04-10 05:14:09 +0200

    testing

--- backend/crowdfunding/api/views.py
+++ backend/crowdfunding/api/views.py
@@ -1,51 +1,52 @@
+from api.models import Category, Project, Rate, User
+from api.modelserializers import (
+    CategorySerializer,
+    CommentSerializer,
+    LoginSerializer,
+    ProjectSerializer,
+    RateSerializer,
+    ReplaySerializer,
+    ReportCommentListCreateAPIView,
+    UserSerializer,
+)
+from api.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly, IsSameUserOrReadOnly
+from comment.models import Comment
+from comment_report.models import Report_comment
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.response import Response
-
-from api.models import User, Category , Project , Rate
-
-from api.modelserializers import UserSerializer,LoginSerializer,CategorySerializer , ProjectSerializer,RateSerializer ,CommentSerializer,ReplaySerializer,ReportCommentListCreateAPIView 
-
-from rest_framework.permissions import IsAuthenticated , IsAuthenticatedOrReadOnly,AllowAny
-
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.viewsets import ModelViewSet
-
-from comment.models import Comment
 from replay.models import Replay
-from comment_report.models import Report_comment
-
-from api.permissions import IsOwnerOrReadOnly, IsSameUserOrReadOnly,IsAdminOrReadOnly
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
-from django.contrib.auth import authenticate
-
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 
 class login(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         print(request.data)
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email
-        })
+        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+
 
 class UserModelViewSet(ModelViewSet):
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsSameUserOrReadOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
 
 
 class CategoryModelViewSet(ModelViewSet):
@@ -56,7 +57,6 @@ class CategoryModelViewSet(ModelViewSet):
     serializer_class = CategorySerializer
 
 
-
 class ProjectModelViewSet(ModelViewSet):
     authentication_classes = [TokenAuthentication]
     # permission_classes = [IsOwnerOrReadOnly]
@@ -65,30 +65,32 @@ class ProjectModelViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
 
 
-
-
 class RateModelViewSet(ModelViewSet):
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
+
+
 class CategoryListCreateAPIView(ListCreateAPIView):
     # permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class CommentListCreateAPIView(ListCreateAPIView):
-    queryset =Comment.objects.all()
-    serializer_class=CommentSerializer
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
 
 class ReplayListCreateAPIView(ListCreateAPIView):
-    queryset =Replay.objects.all()
-    serializer_class=ReplaySerializer
+    queryset = Replay.objects.all()
+    serializer_class = ReplaySerializer
 
 
 class ReportCommentListCreateAPIView(ListCreateAPIView):
-    queryset =Report_comment.objects.all()
-    serializer_class=ReportCommentListCreateAPIView
+    queryset = Report_comment.objects.all()
+    serializer_class = ReportCommentListCreateAPIView
 
 
 from django.core.mail import send_mail
@@ -96,13 +98,13 @@ from django.http import HttpResponse
 
 
 def send_test_email(request):
-    subject = 'Test Email'
-    message = 'This is a test email sent from Django using Gmail SMTP.'
-    from_email = 'amr.abdullah.elrefaey@gmail.com'  # Use your Gmail address here
-    to_email = 'ef64b54e13@emailbbox.pro'
+    subject = "Test Email"
+    message = "This is a test email sent from Django using Gmail SMTP."
+    from_email = "amr.abdullah.elrefaey@gmail.com"  # Use your Gmail address here
+    to_email = "ef64b54e13@emailbbox.pro"
 
     try:
         send_mail(subject, message, from_email, [to_email])
-        return HttpResponse('Test email sent successfully!')
+        return HttpResponse("Test email sent successfully!")
     except Exception as e:
-        return HttpResponse(f'Failed to send test email: {str(e)}')
+        return HttpResponse(f"Failed to send test email: {str(e)}")
--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,174 +1,227 @@
 <template>
- <div class="login vh-100 row justify-content-center align-items-center ">
-<div class="loginCard w-50 h-75 bg-white  row flex-column   ">
+  <div class="login vh-100 row justify-content-center align-items-center">
+    <div class="loginCard w-50 h-100 bg-white row flex-column">
+      <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+        <li class="nav-item" role="presentation">
+          <router-link to="/login"
+            ><button
+              class="nav-link"
+              id="tab-register"
+              data-mdb-toggle="pill"
+              role="tab"
+              aria-controls="pills-register"
+              aria-selected="false"
+            >
+              Login
+            </button></router-link
+          >
+        </li>
 
-<ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-  <li class="nav-item" role="presentation">
-    <router-link to="/login"><button class="nav-link " id="tab-register" data-mdb-toggle="pill" role="tab"
-      aria-controls="pills-register" aria-selected="false">Login</button></router-link>
-     
-  </li>
-  
-  <li class="nav-item" role="presentation">
-    <router-link to="/registration"><button class="nav-link " id="tab-register" data-mdb-toggle="pill" role="tab"
-      aria-controls="pills-register" aria-selected="false">Register</button></router-link>
-  </li>
-</ul>
-<!-- Pills navs -->
+        <li class="nav-item" role="presentation">
+          <router-link to="/registration"
+            ><button
+              class="nav-link"
+              id="tab-register"
+              data-mdb-toggle="pill"
+              role="tab"
+              aria-controls="pills-register"
+              aria-selected="false"
+            >
+              Register
+            </button></router-link
+          >
+        </li>
+      </ul>
+      <!-- Pills navs -->
 
-<!-- Pills content -->
-<div class="tab-content">
-  <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-    <form>
-      <div class="text-center mb-3">
-        <p>Sign in with:</p>
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-facebook-f"></i>
-        </button>
+      <!-- Pills content -->
+      <div class="tab-content">
+        <div
+          class="tab-pane fade show active"
+          id="pills-login"
+          role="tabpanel"
+          aria-labelledby="tab-login"
+        >
+          <form>
+            <div class="text-center mb-3">
+              <p>Sign in with:</p>
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-google"></i>
-        </button>
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-google"></i>
+              </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-twitter"></i>
-        </button>
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-twitter"></i>
+              </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-github"></i>
-        </button>
-      </div>
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-github"></i>
+              </button>
+            </div>
 
-      <p class="text-center">or:</p>
+            <p class="text-center">or:</p>
 
-      <!-- Email input -->
-      <div class="form-outline mb-4">
-        <input type="email" id="loginName" class="form-control" />
-        <label class="form-label" for="loginName">Email or username</label>
-      </div>
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+              <input type="email" id="loginName" class="form-control" />
+              <label class="form-label" for="loginName"
+                >Email or username</label
+              >
+            </div>
 
-      <!-- Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="loginPassword" class="form-control" />
-        <label class="form-label" for="loginPassword">Password</label>
-      </div>
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+              <input type="password" id="loginPassword" class="form-control" />
+              <label class="form-label" for="loginPassword">Password</label>
+            </div>
 
-      <!-- 2 column grid layout -->
-      <div class="row mb-4">
-        <div class="col-md-6 d-flex justify-content-center">
-          <!-- Checkbox -->
-          <div class="form-check mb-3 mb-md-0">
-            <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked />
-            <label class="form-check-label" for="loginCheck"> Remember me </label>
-          </div>
+            <!-- 2 column grid layout -->
+            <div class="row mb-4">
+              <div class="col-md-6 d-flex justify-content-center">
+                <!-- Checkbox -->
+                <div class="form-check mb-3 mb-md-0">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="loginCheck"
+                    checked
+                  />
+                  <label class="form-check-label" for="loginCheck">
+                    Remember me
+                  </label>
+                </div>
+              </div>
+
+              <div class="col-md-6 d-flex justify-content-center">
+                <!-- Simple link -->
+                <a href="#!">Forgot password?</a>
+              </div>
+            </div>
+
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-primary btn-block mb-4">
+              Sign in
+            </button>
+
+            <!-- Register buttons -->
+            <div class="text-center">
+              <p>Not a member? <a href="#!">Register</a></p>
+            </div>
+          </form>
         </div>
+        <div
+          class="tab-pane fade"
+          id="pills-register"
+          role="tabpanel"
+          aria-labelledby="tab-register"
+        >
+          <form>
+            <div class="text-center mb-3">
+              <p>Sign up with:</p>
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </button>
 
-        <div class="col-md-6 d-flex justify-content-center">
-          <!-- Simple link -->
-          <a href="#!">Forgot password?</a>
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-google"></i>
+              </button>
+
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-twitter"></i>
+              </button>
+
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fab fa-github"></i>
+              </button>
+            </div>
+
+            <p class="text-center">or:</p>
+
+            <!-- Name input -->
+            <div class="form-outline mb-4">
+              <input type="text" id="registerName" class="form-control" />
+              <label class="form-label" for="registerName">Name</label>
+            </div>
+
+            <!-- Username input -->
+            <div class="form-outline mb-4">
+              <input type="text" id="registerUsername" class="form-control" />
+              <label class="form-label" for="registerUsername">Username</label>
+            </div>
+
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+              <input type="email" id="registerEmail" class="form-control" />
+              <label class="form-label" for="registerEmail">Email</label>
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+              <input
+                type="password"
+                id="registerPassword"
+                class="form-control"
+              />
+              <label class="form-label" for="registerPassword">Password</label>
+            </div>
+
+            <!-- Repeat Password input -->
+            <div class="form-outline mb-4">
+              <input
+                type="password"
+                id="registerRepeatPassword"
+                class="form-control"
+              />
+              <label class="form-label" for="registerRepeatPassword"
+                >Repeat password</label
+              >
+            </div>
+
+            <!-- Checkbox -->
+            <div class="form-check d-flex justify-content-center mb-4">
+              <input
+                class="form-check-input me-2"
+                type="checkbox"
+                value=""
+                id="registerCheck"
+                checked
+                aria-describedby="registerCheckHelpText"
+              />
+              <label class="form-check-label" for="registerCheck">
+                I have read and agree to the terms
+              </label>
+            </div>
+
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-primary btn-block mb-3">
+              Sign in
+            </button>
+          </form>
         </div>
       </div>
-
-      <!-- Submit button -->
-      <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-      <!-- Register buttons -->
-      <div class="text-center">
-        <p>Not a member? <a href="#!">Register</a></p>
-      </div>
-    </form>
+    </div>
   </div>
-  <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-    <form>
-      <div class="text-center mb-3">
-        <p>Sign up with:</p>
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-facebook-f"></i>
-        </button>
+</template>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-google"></i>
-        </button>
+<script>
+export default {};
+</script>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-twitter"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-github"></i>
-        </button>
-      </div>
-
-      <p class="text-center">or:</p>
-
-      <!-- Name input -->
-      <div class="form-outline mb-4">
-        <input type="text" id="registerName" class="form-control" />
-        <label class="form-label" for="registerName">Name</label>
-      </div>
-
-      <!-- Username input -->
-      <div class="form-outline mb-4">
-        <input type="text" id="registerUsername" class="form-control" />
-        <label class="form-label" for="registerUsername">Username</label>
-      </div>
-
-      <!-- Email input -->
-      <div class="form-outline mb-4">
-        <input type="email" id="registerEmail" class="form-control" />
-        <label class="form-label" for="registerEmail">Email</label>
-      </div>
-
-      <!-- Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="registerPassword" class="form-control" />
-        <label class="form-label" for="registerPassword">Password</label>
-      </div>
-
-      <!-- Repeat Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="registerRepeatPassword" class="form-control" />
-        <label class="form-label" for="registerRepeatPassword">Repeat password</label>
-      </div>
-
-      <!-- Checkbox -->
-      <div class="form-check d-flex justify-content-center mb-4">
-        <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
-          aria-describedby="registerCheckHelpText" />
-        <label class="form-check-label" for="registerCheck">
-          I have read and agree to the terms
-        </label>
-      </div>
-
-      <!-- Submit button -->
-      <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
-    </form>
-  </div>
-</div>
-</div>
-</div>
-  </template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  background-image: url("../assets/imges/pexels-branimir-balogović-3959485.jpg");
+  background-position: center;
+  background-size: cover;
   
-  <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String
-    }
-  }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  .login{
-    background-image: url("../assets/imges/pexels-branimir-balogović-3959485.jpg");
-    background-position: center ;
-    background-size: cover ;
-    
-  }
-  .loginCard{border-radius: 10%;
+}
+.loginCard {
+  border-radius: 10%;
   opacity: 0.8;
-  color:black
-  }
-  </style>
-  
+  color: black;
+}
+</style>
