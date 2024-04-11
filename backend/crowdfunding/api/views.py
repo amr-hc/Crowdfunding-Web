8c20a8d152b4db708561b11511120197c805abe7
dabour1 <Aldabor63@Yahoo.Com>
2024-04-11 03:18:38 +0200

    ....

--- backend/crowdfunding/api/views.py
+++ backend/crowdfunding/api/views.py
@@ -45,6 +45,7 @@ class login(ObtainAuthToken):
 class UserModelViewSet(ModelViewSet):
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsSameUserOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
