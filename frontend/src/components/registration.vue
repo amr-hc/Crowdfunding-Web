7abb5edae233d861d7ec1013756bedafbce7347c
dabour1 <Aldabor63@Yahoo.Com>
2024-04-12 21:41:53 +0200

    linkUpdateProfileToAPI

--- frontend/src/components/registration.vue
+++ frontend/src/components/registration.vue
@@ -153,8 +153,10 @@
 </template>
 
 <script>
+import FunctionsClass from '../assets/js/registerAndUpdate'
+   const functionsObject=new FunctionsClass();
 export default {
-    data:()=>({
+  data:()=>({
       fname:'',
       lname:'',
       email:'',
@@ -167,155 +169,25 @@ export default {
       file:null,
       countries: []
       }),
-      async created() {
-    try {
-        const response = await fetch('https://countriesnow.space/api/v0.1/countries/codes');
-        const data = await response.json();
-        this.countries=data.data.map((data)=>{
-          return data.name
-        })
-         
-    } catch (error) {
-        console.error("Error fetching country codes:", error);
-    }
-},
-  methods:{
-    HTMLValidations(e)
-    {
-      if (!e.target.checkValidity())
-       {
-          e.preventDefault();
-          e.stopPropagation();
-          e.target.classList.add("was-validated");
-          return false;
-        }
-        else
-        {
-          e.target.classList.add("was-validated");
-          return true;
-        }
-    },
-    jsValidations()
-    {
-      
-        const namePattern = /^[a-zA-Z ,.'-]+$/;
-        const emailPattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
-        const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-        const mobilePattern = /^[1-9]\d*$/;
-        const countryPattern = /^[a-zA-Z ,.'-]+$/;
-        const birthdatePattern = /^(((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))[-/]?[0-9]{4}|02[-/]?29[-/]?([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00))$/;
-        const facebookPattern = /^(?:https?:\/\/)?(?:www\.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/;
-        if(this.country!="")
-        {
-          if(countryPattern.test(this.country))
-              {
-                return true;
-              }
-          else
-              {
-              
-                return false;
-               
-
-              }
-        }
-        if(this.birthdate!="")
-        {
-          if(birthdatePattern.test(this.birthdate))
-            {
-              return true;
-            }
-          else
-            {
-            
-           
-              return false;
-            }
-        }
-        if(this.facebook!="")
-        {
-          if(facebookPattern.test(this.facebook))
-              {
-                return true;
-              }
-          else
-              {
-                
-              
-                return false;
-              }
-        }
-        if(
-        namePattern.test(this.fname)
-        &&namePattern.test(this.lname)
-        &&emailPattern.test(this.email)
-        &&passwordPattern.test(this.password)
-        &&mobilePattern.test(this.mobile)    
-          )
-        {
-          return true;
-        }
-        else
-        {
-          
-          return false;
-        }
-    },
+	  created(){
+		functionsObject.created(this)
+	  },
+	  methods:{
+		handleFormSubmission(e)
+		{ 
+			functionsObject.handleFormSubmission(e,this)
+        },
     confirm(e){
-      if(this.cpassword!=this.password)
-      {
-        e.target.setCustomValidity("Passwords don't match");
-      }
-      else
-      {
-        e.target.setCustomValidity('');
-      }
-    },
-  handleFileChange(event)
-    {
-      this.file = event.target.files[0];
-    },
-     
+			functionsObject.confirm(e,this);	
 
- async sendrequest()
-    {
-       
-        const formData = new FormData();
-        formData.append('first_name', this.fname);
-        formData.append('last_name',this.lname);
-        formData.append('email', this.email);
-        formData.append('password', this.password);
-        formData.append('phone', this.mobile);
-        formData.append('birth_date', this.birthdate);
-        // formData.append('facebook', this.facebook);
-        //  formData.append('country', this.country);
-        // formData.append('photo', this.file);
-        try 
-        {
-          
-              const response = await fetch('http://127.0.0.1:8000/auth/users/',{
-            method: "POST",
-            body: formData,
-          });
-              const data = await response.json(); 
-            console.log(data)
-        }
-      catch (error) 
-          {
-              console.error("Error fetching country codes:", error);
-          }
-    },
-
-    handleFormSubmission(e) 
-      {
-        if(this.HTMLValidations(e)&&this.jsValidations())
-        {
-          
-          this.sendrequest();
-        }
+		},
+		handleFileChange(e){
+			functionsObject.handleFileChange(e,this)
+		}
       },
-    }
-}
+		
+    
+		}
 </script>
 
 
--- frontend/src/components/updateUserProfile.vue
+++ frontend/src/components/updateUserProfile.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
 		<div class="main-body">
-			<form action=""> 
+			<form  @submit.prevent="handleFormSubmission($event);" class="needs-validation" novalidate> 
 			<div class="row">
 				 <div class="col-lg-4">
 					<div class="card">
@@ -11,7 +11,9 @@
 									<div   class=" imgdiv rounded-circle p-1 bg-primary"  > 
 											
 										<label class="rounded-5 rounded-top-0" for="inputimg"><i class="fa fa-2x fa-camera" aria-hidden="true"></i><br> new image</label>
-											<input type="file" id="inputimg">
+											<input type="file" id="inputimg" @change="handleFileChange">
+											<div class="invalid-feedback">Please select a vaild image!</div>
+
 									</div>
 
                                 </div>
@@ -35,7 +37,11 @@
 									<h6 class="mb-0">First Name</h6>
 								</div>
 								<div class="col-sm-9 text-secondary">
-									<input type="text" class="form-control" pattern="^[a-zA-Z ,.'\-]+$" value="John Doe">
+									<input type="text" v-model="fname" class="form-control" pattern="^[a-zA-Z ,.'\-]+$" value="John Doe">
+									<div class="invalid-feedback">
+										Please enter a valid First name (letters, spaces, commas, periods, single quotes, and hyphens)!.
+										</div>
+								
 								</div>
 							</div>
 							<div class="row mb-3">
@@ -43,7 +49,11 @@
 									<h6 class="mb-0">Last Name</h6>
 								</div>
 								<div class="col-sm-9 text-secondary">
-									<input type="text" class="form-control" pattern="^[a-zA-Z ,.'\-]+$" value="John Doe">
+									<input type="text"  v-model="lname" class="form-control" pattern="^[a-zA-Z ,.'\-]+$" value="John Doe">
+								
+									<div class="invalid-feedback">
+										Please enter a valid Last name (letters, spaces, commas, periods, single quotes, and hyphens)!.
+									</div>
 								</div>
 							</div>
 							<div class="row mb-3">
@@ -51,7 +61,11 @@
 									<h6 class="mb-0">Email</h6>
 								</div>
 								<div class="col-sm-9 text-secondary">
-									<input type="email" class="form-control"  pattern="[^@\s]+@[^@\s]+\.[^@\s]+" value="john@example.com">
+									<input type="email" class="form-control" v-model="email"  pattern="[^@\s]+@[^@\s]+\.[^@\s]+" value="john@example.com">
+								
+									<div class="invalid-feedback">
+										Please enter a valid email!.
+										</div>
 								</div>
 							</div>
 							<div class="row mb-3">
@@ -59,7 +73,32 @@
 									<h6 class="mb-0">Phone</h6>
 								</div>
 								<div class="col-sm-9 text-secondary">
-									<input type="number" class="form-control"  pattern="^[1-9]\d*$" value="(239) 816-9029">
+									<input type="number" class="form-control" v-model="mobile"  pattern="^[1-9]\d*$" value="(239) 816-9029">
+									<div class="invalid-feedback">
+										Please enter user Mobile no.!.
+										</div>
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">password</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="password" class="form-control" v-model="password"  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" value="(239) 816-9029">
+									<div class="invalid-feedback">
+										Please enter a valid password Minimum eight characters, at least one letter and one number!.
+										</div>
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Confirm password</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="password" class="form-control"  v-model="cpassword" @blur="confirm"   pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" value="(239) 816-9029">
+									<div class="invalid-feedback">
+										Please enter a valid password that matchs with previous input  !.
+										</div>
 								</div>
 							</div>
 							<div class="row mb-3">
@@ -72,6 +111,9 @@
 										<option v-for="country in countries" :key="country ">{{ country}}</option>
 										<option> </option>
 									</select>
+									<div class="invalid-feedback">
+										Please select a valid state.
+										</div>
 								</div>
 							</div>
 							<div class="row mb-3">
@@ -80,8 +122,12 @@
 								</div>
 								<div class="col-sm-9 text-secondary">
 									<input type="date" class="form-control"
+									v-model="birthdate"
 									pattern="^(((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))[-/]?[0-9]{4}|02[-/]?29[-/]?([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00))$"
 									value="22-09-1997">
+									<div class="invalid-feedback">
+										Please provide a valid Birthdate.
+										</div>
 								</div>
 							</div>
 							<div class="row mb-3">
@@ -90,9 +136,12 @@
 								</div>
 								<div class="col-sm-9 text-secondary">
 									<input type="text"
+									v-model="facebook"
 									 class="form-control"
 									 pattern="(?:https?:\/\/)?(?:www\.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)"
 									  value="www.facebook.com">
+									  <div class="invalid-feedback">
+                                        Please provide a valid Facebook account.</div>
 								</div>
 							</div>
 							<div class="row">
@@ -114,9 +163,42 @@
   </template>
   
   <script>
+  import FunctionsClass from '../assets/js/registerAndUpdate'
+   const functionsObject=new FunctionsClass();
   export default {
+	data:()=>({
+      fname:'',
+      lname:'',
+      email:'',
+      mobile:'',
+      password:'',
+      cpassword:'',
+      country:'',
+      birthdate:'',
+      facebook:'',
+      file:null,
+      countries: []
+      }),
+	  created(){
+		functionsObject.created(this)
+	  },
+	  methods:{
+		handleFormSubmission(e)
+		{ 
+			functionsObject.handleFormSubmission(e,this)
+        },
+		confirm(e){
+			functionsObject.confirm(e,this);	
+
+		},
+		handleFileChange(e){
+			functionsObject.handleFileChange(e,this)
+		}
+	  }
+		}
+	  
+
   
-  }
   </script>
   
   <style>
