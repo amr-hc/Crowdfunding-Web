9ee95f6d1044f6e7a013917d208fff416e21cb15
amr-hc <amro7275@gmail.com>
2024-04-15 00:02:24 +0200

    edit url active link

--- backend/crowdfunding/api/views.py
+++ backend/crowdfunding/api/views.py
@@ -55,9 +55,7 @@ class login(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key, "user_id": user.pk, "email": user.email, "first_name": user.first_name,
-                         "last_name": user.last_name, "photo": user.photo.url, "is_superuser": user.is_superuser,
-                         "country": user.country, "facebook": user.facebook, "address": user.address})
+        return Response({"token": token.key, "user_id": user.pk, "is_superuser": user.is_superuser})
 
 
 class UserModelViewSet(ModelViewSet):
@@ -76,8 +74,7 @@ class UserModelViewSet(ModelViewSet):
         print(uid)
         print(token)
         subject = "Confirm account Crowdfunding"
-        domain = get_current_site(self.request).domain
-        reset_link = f"http://{domain}/reset-password/{uid}/{token}/"
+        reset_link = f"http://localhost:8080/congs?uid={uid}&token={token}"
         message = f"welcome to Crowdfunding, to confirm your new account please click on <a href=\"{reset_link}\">Click here</a>"
         from_email = "amr.abdullah.elrefaey@gmail.com"
         to_email = user.email
