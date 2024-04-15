af0d3f98a5a74bb4ac5486bdf9a8c0a65787b423
dabour1 <Aldabor63@Yahoo.Com>
2024-04-15 16:39:00 +0200

    fix conflect

--- frontend/src/stors/crowdfundingStore.js
+++ frontend/src/stors/crowdfundingStore.js
@@ -9,41 +9,11 @@ export const datastore = defineStore("crowdfunding", {
         const response = await fetch(`http://127.0.0.1:8000/api/users/${id}`, {
           method: "GET",
           headers: {
-<<<<<<< HEAD
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token} `
-          }
-         });
-         const user = await response.json(); 
-         console.log(user)
-         
-         return user
-          
-    
-    
-        }
-      catch (error) 
-          {
-              console.error("Error fetching country codes:", error);
-          }
-             
-                
-            
-        },
-          
-    },
-    
-getters:{
-    
-   
-}
-=======
             "Content-Type": "application/json",
             Authorization: `Bearer ${token} `,
           },
         });
         const user = await response.json();
->>>>>>> 2e8fcbf20cdfe283cc02a22ed7f5e2ae3e23afb9
 
         return user;
       } catch (error) {
