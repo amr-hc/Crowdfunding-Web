56a56c9c5beb450c660b3117a27159d86ffd6b62
amr-hc <amro7275@gmail.com>
2024-04-15 21:52:45 +0200

    edit permission all api

--- backend/crowdfunding/api/permissions.py
+++ backend/crowdfunding/api/permissions.py
@@ -2,43 +2,40 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "GET" or request.user.is_authenticated:
+            return True
+        return False
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
+        if request.method == "GET" or request.user.is_superuser:
             return True
         return obj.owner == request.user
 
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
-            return True
-        return False
 
 
 class IsSameUserOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
-            return True
-        elif "is_superuser" in request.data or "is_active" in request.data:
-            return False
-        return obj == request.user
-
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
-            return True
-        elif "is_superuser" in request.data or "is_active" in request.data:
+        if not request.user.is_superuser and ("is_superuser" in request.data or "is_active" in request.data):
             return False
         return True
 
+    def has_object_permission(self, request, view, obj):
+        if request.method == "GET" or (request.method == "PATCH" and obj == request.user) or request.user.is_superuser:
+            return True
+        return False
+
+
+
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
-            return True
-        return False
-
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
+        if request.method == "GET" or request.user.is_superuser:
             return True
         return False
+    def has_object_permission(self, request, view, obj):
+        return True
+
+
 
 class IsOwnerCommentOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
