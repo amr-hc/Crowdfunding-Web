52342799b513578b2d577ffd2095556e9349ce53
Blitz576 <anagy8050@gmail.com>
2024-04-20 06:38:43 +0200

    fixed issue in comment model

--- backend/crowdfunding/comment/models.py
+++ backend/crowdfunding/comment/models.py
@@ -3,7 +3,7 @@ from api.models import User
 from api.models import Project
 
 class Comment(models.Model):
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Project, related_name="comments",on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.CharField( max_length=500,null=True, blank=True)
 
