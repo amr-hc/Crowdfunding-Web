6322764803c638370786ca1200c7bc1455118bf6
amr-hc <amro7275@gmail.com>
2024-04-23 18:01:34 +0200

    run permissions

--- backend/crowdfunding/comment_report/views.py
+++ backend/crowdfunding/comment_report/views.py
@@ -1,18 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.viewsets import ModelViewSet
-from api.permissions import IsOwnerOrReadOnly, IsAdminOrpost, IsOwnerOrAdmin
+from api.permissions import IsAdminOrpost, IsOwnerOrAdmin
 from comment_report.models import Report_comment
 from api.modelserializers import ReportCommentSerializer
-from rest_framework.permissions import (
-    AllowAny,
-    IsAuthenticated,
-    IsAuthenticatedOrReadOnly,
-)
-# Create your views here.
-class CommentReportModelViewSet(ModelViewSet):
-    permission_classes = [IsOwnerOrReadOnly]
-    queryset = Report_comment.objects.all()
-    serializer_class = ReportCommentSerializer
 
 
 
